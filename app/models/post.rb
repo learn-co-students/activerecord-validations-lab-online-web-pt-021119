e195f04e2a3a848c04015625f2e853f54a1e6261
github username <github email address>
2019-06-28 14:28:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class MyValidator < ActiveModel::Validator
+  def validate(title)
+    unless title.starts_with? 'X'
+      record.errors[:name] << 'Need a category starting with X please!'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
