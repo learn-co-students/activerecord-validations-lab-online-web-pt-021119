2810522d1548858f662fe958a97feca2561cf9c3
github username <github email address>
2019-06-28 15:46:48 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.title.starts_with? 'X'
-      record.errors[:title] << 'Need a category starting with X please!'
-    end
-  end
-end
 
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with MyValidator
+  validate :click_baity_title, on: :create
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+  def click_baity_title
+    if self.title && !self.title.match?(/(Won't\sBelieve)|(Secret)|(Top\d+)|(Guess)/)
+      errors.add(:title, "Doesn't have an h")
+    end
+  end
 end
